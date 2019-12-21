18c1573888a14d941a32f4f9b891cfdf33407620
Majid <a@yahoo.com>
2019-12-21 12:18:00 +0330

    These functions defined in func1.py 	avg(a,b) 	sum(a,b) Theses class defined in func1.py 	math_class
    Printing sum and avg of two numbers added to main.py

---
+++ func1.py
@@ -0,0 +1,17 @@
+def sum(a,b):
+    return(a+b)
+def avg(a,b):
+    return((a+b)/2)
+
+class math_class:
+    def __init__(self,a,b):
+        self.mem1 = a
+        self.mem2 = b
+    def sum(self):
+        return(self.mem1 + self.mem2)
+    def avg(self):
+        return((self.mem1+self.mem2)/2)
+
+
+
+
--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-Line1
+import func1
+
+m = func1.math_class(10,4)
+print("{} + {} = {}".format(m.mem1,m.mem2,m.sum()))
+print("AVG({},{}) = {}".format(m.mem1,m.mem2,m.avg()))
+print(func1.avg(m.mem1,m.mem2))
