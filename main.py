5e7e8759c6021f2c3c78c50723448d3ba1c60251
Majid <a@yahoo.com>
2019-12-21 12:28:37 +0330

    Added some extra print instructions to main.py

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ import func1
 m = func1.math_class(10,4)
 print("{} + {} = {}".format(m.mem1,m.mem2,m.sum()))
 print("AVG({},{}) = {}".format(m.mem1,m.mem2,m.avg()))
-print(func1.avg(m.mem1,m.mem2))
+print("SUM: "+str(func1.sum(m.mem1,m.mem2)))
+print("AVG: "+str(func1.avg(m.mem1,m.mem2)))
