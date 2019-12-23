3b79609f0461e2773678aee899f9698810c95633
Majid <a@yahoo.com>
2019-12-23 09:38:00 +0330

    func3.py added to projrct and used in main.py

---
+++ func3.py
@@ -0,0 +1,6 @@
+def mul(a,b):
+    return(a,b)
+
+class m3:
+    def __init__(self,str1,str2):
+        print(str1+" "+str2)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import func1
 import func2
+import func3
 
 m = func1.math_class(10,4)
 print("{} + {} = {}".format(m.mem1,m.mem2,m.sum()))
@@ -7,3 +8,6 @@ print("AVG({},{}) = {}".format(m.mem1,m.mem2,m.avg()))
 print("SUM: "+str(func1.sum(m.mem1,m.mem2)))
 print("AVG: "+str(func1.avg(m.mem1,m.mem2)))
 print("9-2 = ",func2.sub(9,2))
+del m
+m = func3.m3("This is","func3")   
+del m
