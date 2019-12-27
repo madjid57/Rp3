8b4efe6e8b3f5acd367cc107227b95f477d86734
Majid <a.yahoo.com>
2019-12-27 10:18:05 +0330

    Some lines removed from main.py

--- main.py
+++ main.py
@@ -13,13 +13,3 @@ del m
 m = func2.func2_class
 m.about()
 del m
-m = [[1,2],[2,3]]
-print(m)
-print(m[0])
-print(m[1])
-print(m[0:2])
-del m
-m = ((1,2),(3,4))
-m1 = list(m)
-print(m*2)
-print("change on BR1")
